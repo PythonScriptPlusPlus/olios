9744e5ab2e9c5b6bdec0c45ecc21e8576a91943f
George <gsemmail@gmail.com>
2021-08-15 16:35:49 +0300

    styles and button class were added

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Items from '../views/Items.vue';
+import Items from '../views/ItemsPage.vue';
 import MainPage from '../views/MainPage.vue';
 import SearchPage from '../views/SearchPage.vue';
 import ShopCart from '../views/ShoppingCart.vue';
 import Page404 from '../views/Page404.vue';
+import GUI from '../views/GuiPage.vue';
 
 Vue.use(VueRouter);
 
@@ -33,6 +34,11 @@ const routes = [
     path: '*',
     name: 'PageNotFound',
     component: Page404,
+  },
+  {
+    path: '/gui',
+    name: 'GUI',
+    component: GUI,
   },
 ];
 
---
+++ src/views/GuiPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <!--<h1>GUI page</h1> -->
+        <button class="btn">
+            click me
+        </button>
+        <ol-input />
+    </div>
+</template>

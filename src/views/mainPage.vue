3c5611bb227f6fc8d75dd866ab65f99d933ab973
George <gsemmail@gmail.com>
2021-08-27 15:34:48 +0300

    work on main page

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -1,6 +1,16 @@
 <template>
 	<div>
-		<h1>Best landing page on the internet</h1>
+		<div class="header"> 
+			<p class="header__title">
+				olios
+			</p>
+			<p class="header__subtitle">
+				newest furniture shop existing
+			</p>
+			<button class="btn">
+				view more
+			</button>
+		</div>
 	</div>
 </template>
 
@@ -9,3 +19,26 @@ export default {
 	name: 'MainPage',
 };
 </script>
+<style lang="scss" scoped>
+.header {
+	margin: 0 auto 0 auto;
+	padding-top: 75px;
+	height: 100vh;
+	text-transform: uppercase;
+	background-image: url(../../public/img/background.png);
+	background-size: cover;
+	background-position-y: bottom;
+
+	&__title {
+		font-size: 108px;
+    	font-weight: 500;
+		margin: 0;
+	}
+
+	&__subtitle {
+		margin: 40px 0px 10px 0px;
+		font-size: 18px;
+	}
+}
+
+</style>

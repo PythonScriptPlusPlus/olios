27d42d8155b6a9c5f8d9977a2c287cae0a6ed53e
George <gsemmail@gmail.com>
2021-09-21 20:53:20 +0300

    shopping cart, order and promo cards were made

--- src/views/shoppingCart.vue
+++ src/views/shoppingCart.vue
@@ -1,6 +1,21 @@
 <template>
-	<div>
-		<h1>Sorry, you don't have anything in Shopping Cart</h1>
+	<div class="container page">
+		<h1 class="title">
+			Shopping Cart
+		</h1>
+		<div class="wrapper">
+			<div class="shop-cart">
+				shpcart
+			</div>
+			<div class="wrap">
+				<div class="delivery">
+					order
+				</div>
+				<div class="promo">
+					promo
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -9,3 +24,42 @@ export default {
 	name: 'ShoppingCart',
 };
 </script>
+
+<style lang="scss" scoped>
+.page {
+	text-align: left;
+	font-size: 20px;
+}
+
+.title {
+	font-weight: 200;
+}
+
+.wrapper {
+	display: flex;
+}
+
+.shop-cart {
+	width: 72%;
+	height: 600px;
+	box-shadow: 0 0 15px $sub-text;
+	margin-right: 3%;
+}
+
+.wrap {
+	width: 25%;
+	display: flex;
+	flex-direction: column;
+}
+
+.delivery {
+	height: 400px;
+	box-shadow: 0 0 15px $sub-text;
+	margin-bottom: 30px;
+}
+
+.promo {
+	height: 170px;
+	box-shadow: 0 0 15px $sub-text;
+}
+</style>

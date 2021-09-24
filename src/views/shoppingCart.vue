2c7c81ad8521c1b93f90300032e681a97d4e5a19
George <gsemmail@gmail.com>
2021-09-24 18:31:34 +0300

    some elements are changed to blocks

--- src/views/shoppingCart.vue
+++ src/views/shoppingCart.vue
@@ -4,13 +4,13 @@
 			Shopping Cart
 		</h1>
 		<div class="cart__wrapper">
-			<div class="cart__wrapper__shop-cart">
+			<div class="shop-card">
 				shpcart
 			</div>
 			<div class="info">
 				<p>amount of items: {{ amount }}</p>
 				<p class="info__price">
-					total: <span class="info__price--cost">{{ price }}$</span>
+					total: <span class="info__cost">{{ price }}$</span>
 				</p>
 			</div>
 			<div class="cart__wrapper side-bar">
@@ -49,14 +49,14 @@ export default {
 	&__wrapper {
 		display: flex;
 		position: relative;
-
-		&__shop-cart {
-			width: 72%;
-			height: 600px;
-			box-shadow: 0 0 15px $sub-text;
-			margin-right: 3%;
-		}
 	}
+}
+
+.shop-card {
+	width: 72%;
+	height: 600px;
+	box-shadow: 0 0 15px $sub-text;
+	margin-right: 3%;
 }
 
 .info {
@@ -66,14 +66,17 @@ export default {
 	left: 20px;
 	display: flex;
 	justify-content: space-between;
+	
 	&__price {
 		font-weight: 600;
 		text-transform: uppercase;
-		&--cost {
+	}
+
+	&__cost {
 			font-weight: 200;
 			font-style: italic;
-		}
 	}
+	
 }
 
 .side-bar {
